57fcb3ee79b0e8414aa2096b7509c8ec00bfb2e5
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-01-02 16:33:21 -0700

    Finish exercise 3.2

--- index.js
+++ index.js
@@ -4,32 +4,41 @@ const app = express();
 app.use(express.json());
 
 const persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const infoPageCode = `<div><p>Phonebook has info for ${
+  persons.length
+} people.</p><p>${new Date()}</p></div>`;
 
 // HTTP GET route for all persons
-app.get('/api/persons', (request, response) => {
-    response.json(persons);
-})
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+// HTTP GET route for info page
+app.get("/info", (req, res) => {
+  res.send(infoPageCode);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
